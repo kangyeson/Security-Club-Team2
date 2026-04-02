23d689e1009e7ca4c7be0f8ec72187e76b440cf3
kangyeson <yesonkang@gmail.com>
2026-04-03 01:24:38 +0900

    [Fix] 로컬환경 테스트 시에도 서버 DB 사용하도록 수정

--- database.py
+++ database.py
@@ -3,18 +3,10 @@ import os
 import pymysql
 
 def get_db_connection():
-    # 서버(도커 내부)에서는 'db'를 보고, 로컬에서는 '127.0.0.1'을 봄
-    # 환경변수 'DEPLOYMENT'가 설정되어 있지 않으면 로컬 모드로 동작
-    if os.environ.get('DEPLOYMENT') == 'SERVER':
-        db_host = 'db'
-        db_port = 3306
-    else:
-        db_host = '127.0.0.1'
-        db_port = 3308  # 로컬 테스트용
 
     return pymysql.connect(
-        host=db_host,
-        port=db_port,
+        host='gachon.arang.kr',
+        port=3306,
         user='root',
         password=os.environ.get('DB_PASS', 'rkcjs123!'),
         database='security_lab',
